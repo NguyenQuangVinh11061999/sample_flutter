4ed6f59b18a12935e5f8146939897767edf5d14d
vinhnq <hault@gem-corp.tech>
2025-05-28 11:39:19 +0700

    code

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 Text(
                   "Explore the best collection’s of NFTs and buy and \n sell your NFTs as well",
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 10),
+                  selectionColor: Colors.amber,
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontFamily: 'Times New Roman',
+                    wordSpacing: 2, // khoang cach chu
+                    letterSpacing: 2, // khoang cach ky tu
+                    decoration: TextDecoration.lineThrough, // kieu chu gach
+                  ),
                 ),
 
                 Container(
